7666f3156560ed6c59ae6c4e2b6e6cf0d64de471
Ian Johnson <ijohnson@covermymeds.com>
2019-06-20 23:30:57 -0400

    Add tests

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use structopt::StructOpt;
 use failure::ResultExt;
 use exitfailure::ExitFailure;
 
-/// Seach for a patter in a file and display the lines that contain it.
+/// Seach for a pattern in a file and display the lines that contain it.
 #[derive(StructOpt)]
 struct Cli {
     /// The pattern to look for
@@ -12,8 +12,8 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-#[derive(Debug)]
-struct CustomError(String);
+// #[derive(Debug)]
+// struct CustomError(String);
 
 // fn main() {
 // fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -49,7 +49,61 @@ fn main() -> Result<(), ExitFailure> {
     // Ok(())
 
     let content = std::fs::read_to_string(&args.path)
-        .with_context(|_| format!("could not read file `{:?}`", &args.path))?;
+        .with_context(|_| format!("could not read file `{}`", args.path.display()))?;
     println!("File content: {}", content);
+
+    grrs::find_matches(&content, &args.pattern, &mut std::io::stdout());
+
     Ok(())
+}
+
+// Using Buffers to Write to the Terminal without performance issues
+
+// use std::io::{self, Write};
+//
+// let stdout = io::stdout(); // get the global stdout entity
+// let mut handle = io::BufWriter::new(stdout); // optional: wrap that handle in a buffer
+// writeln!(handle, "foo: {}", 42); // add `?` if you care about errors here
+
+// use std::io::{self, Write};
+//
+// let stdout = io::stdout(); // get the global stdout entity
+// let mut handle = stdout.lock(); // acquire a lock on it
+// writeln!(handle, "foo: {}", 42); // add `?` if you care about errors here
+
+
+// Progress Bar
+// indicatif crate
+// fn main() {
+//     let pb = indicatif::ProgressBar::new(100);
+//     for i in 0..100 {
+//         do_hard_work();
+//         pb.println(format!("[+] finished #{}", i));
+//         pb.inc(1);
+//     }
+//     pb.finish_with_message("done");
+// }
+
+// Logging
+// use log::{info, warn};
+//
+// fn main() {
+//     env_logger::init();
+//     info!("starting up");
+//     warn!("oops, nothing implemented!");
+// }
+//
+// $ env RUST_LOG=output_log=info cargo run --bin output-log
+
+#[test]
+fn check_answer_validity() {
+    let answer = 42;
+    assert_eq!(answer, 42);
+}
+
+#[test]
+fn find_a_match() {
+    let mut result = Vec::new();
+    grrs::find_matches("lorem ipsum\ndolor sit amet", "lorem", &mut result);
+    assert_eq!(result, b"lorem ipsum\n");
 }
