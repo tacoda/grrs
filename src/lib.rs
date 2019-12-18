c3adf0a4773753b8ac40f72f07f6709bd320693b
Ian Johnson <ijohnson@covermymeds.com>
2019-12-18 18:04:04 -0500

    Update tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+// By convention, cargo will look for integration tests in the tests/
+// directory. Similarly, it will look for benchmarks in benches/, and
+// examples in examples/. These conventions also extend to your main
+// source code: libraries have a src/lib.rs file, the main binary is
+// src/main.rs, or, if there are multiple binaries, cargo expects them
+// to be in src/bin/<name>.rs. Following these conventions will make
+// your code base more discoverable by people used to reading Rust code.
+
 pub fn find_matches(content: &str, pattern: &str, mut writer: impl std::io::Write) {
     for line in content.lines() {
         if line.contains(pattern) {
